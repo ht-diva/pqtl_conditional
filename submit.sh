7d9d031a8d278b08d3bde52e2c4855571fbbc86e
dariushghasemi <ghasemi.dariush@yahoo.com>
2026-03-01 00:57:38 +0100

    Lunch LB for Beleive

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name combine
-#SBATCH --output %j_combine_loci.log
+#SBATCH --job-name lb
+#SBATCH --output %j_believe_lb.log
 #SBATCH --partition cpuq
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 4G
